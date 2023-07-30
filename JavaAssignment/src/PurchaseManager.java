078dfe922c0f76af0087cfbed0747df45a2a1a24
WeiLun0410 <111478365+WeiLun0410@users.noreply.github.com>
2023-07-30 20:43:02 +0800

    Update PurchaseManager.java

--- JavaAssignment/src/PurchaseManager.java
+++ JavaAssignment/src/PurchaseManager.java
@@ -1,5 +1,7 @@
-public class PurchaseManager extends User {
+public class PurchaseManager extends User implements Purchasable{
     public PurchaseManager(String userID, String userPassword, String role, String userName, String contactNumber, String email) {
         super(userID, userPassword, role, userName, contactNumber, email);
     }
+
 }
+
