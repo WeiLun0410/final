fb1be3351d0faa1b1569669b2bf127ee8a476b71
WeiLun0410 <111478365+WeiLun0410@users.noreply.github.com>
2023-07-30 20:44:23 +0800

    Update SalesManager.java

--- JavaAssignment/src/SalesManager.java
+++ JavaAssignment/src/SalesManager.java
@@ -1,4 +1,4 @@
-public class SalesManager extends User{
+public class SalesManager extends User implements Manageable{
     public SalesManager(String userID, String userPassword, String role, String userName, String contactNumber, String email) {
         super(userID, userPassword, role, userName, contactNumber, email);
     }
